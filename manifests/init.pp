5705a0e7ad52a778b772443cc2d79f401128687a
Eric Internicola <einternicola@triton.local>
2013-04-07 12:01:08 -0600

    Added some parameters based on os and fixed the package installation procedures based on whether it's centos or ubuntu.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,19 @@
 class tomcat6 {
  
+  include tomcat6::params
+  include tomcat6::generic
+  
+  case $operatingsystem {
+      'RedHat', 'CentOS': { include tomcat6::centos  }
+      /^(Debian|Ubuntu)$/:{ include tomcat6::debian  }
+    }
+ 
   $tomcat_port = 8080
  
   notice("Establishing http://$hostname:$tomcat_port/")
  
   Package { # defaults
     ensure => installed,
-  }
- 
-  package { 'tomcat6':
-  }
- 
-  package { 'tomcat6-user':
-    require => Package['tomcat6'],
-  }
- 
-  package { 'tomcat6-admin':
-    require => Package['tomcat6'],
   }
  
   file { "/etc/tomcat6/tomcat-users.xml":
@@ -30,8 +27,29 @@ class tomcat6 {
     ensure => running,
     require => Package['tomcat6'],
   }
- 
 }
+
+class tomcat6::generic {
+  package { 'tomcat6':
+  }
+  package { 'tomcat6-admin':
+    require => Package['tomcat6'],
+    name   => $tomcat6::params::tomcat6_admin_package,
+  }
+}
+
+class tomcat6::centos {
+  package { 'tomcat6-webapps':
+    require => Package['tomcat6']
+  }
+}
+
+class tomcat6::debian {
+  package { 'tomcat6-user':
+    require => Package['tomcat6']
+  }
+}
+
 
 define tomcat::deployment($path) {
  
