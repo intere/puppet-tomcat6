ff3a425f587e580a3f0bbdb3e235eb8f894deb85
Eric Internicola <einternicola@triton.local>
2013-05-14 07:50:47 -0600

    Incremented the version of tomcat being used.  It was apache-tomcat-6.0.36, but now it's apache-tomcat-6.0.37.

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,24 +23,24 @@ class tomcat6 {
 	exec {
 		"download_tomcat6" :
 			cwd => "/tmp",
-			command => "/usr/bin/wget http://mirror.nexcess.net/apache/tomcat/tomcat-6/v6.0.36/bin/apache-tomcat-6.0.36.tar.gz",
-			creates => "/tmp/apache-tomcat-6.0.36.tar.gz";
+			command => "/usr/bin/wget http://mirror.olnevhost.net/pub/apache/tomcat/tomcat-6/v6.0.37/bin/apache-tomcat-6.0.37.tar.gz",
+			creates => "/tmp/apache-tomcat-6.0.37.tar.gz";
 
 		"unpack_tomcat6" :
 			cwd => "/opt",
-			command => "/bin/tar -zxf /tmp/apache-tomcat-6.0.36.tar.gz",
-			creates => "/opt/apache-tomcat-6.0.36",
+			command => "/bin/tar -zxf /tmp/apache-tomcat-6.0.37.tar.gz",
+			creates => "/opt/apache-tomcat-6.0.37",
 			require => [ Exec["download_tomcat6"] ];
 	}
 
-	file { "/opt/apache-tomcat-6.0.36" :
+	file { "/opt/apache-tomcat-6.0.37" :
 		recurse => true,
 		owner => 'tomcat',
 		group => 'tomcat',
 		require => [ Exec['unpack_tomcat6'], Class['tomcat6::prereqs'] ]
 	}
  
-  	file { "/opt/apache-tomcat-6.0.36/conf/tomcat-users.xml":
+  	file { "/opt/apache-tomcat-6.0.37/conf/tomcat-users.xml":
     		owner => 'tomcat',
 			group => 'tomcat',
     		require => [ Exec['unpack_tomcat6'], Class['tomcat6::prereqs'] ],
@@ -63,7 +63,7 @@ define tomcat::deployment($path) {
 	include tomcat6
   	notice("Establishing http://$hostname:${tomcat6::tomcat_port}/$name/")
  
-	file { "/opt/apache-tomcat-6.0.36/webapps/${name}.war":
+	file { "/opt/apache-tomcat-6.0.37/webapps/${name}.war":
 		owner => 'tomcat',
 		group => 'tomcat',
 		source => $path,
